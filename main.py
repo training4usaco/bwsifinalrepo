d3fb6aa3168a07db18b14c9a871d0dfe12a4ccf3
aruld <aruldmoorthyrams@gmail.com>
2024-07-22 18:27:33 -0400

    qiskit start

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-print("h")
-print("7")
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
+from qiskit_aer import AerSimulator
+from qiskit import transpile, assemble
+from qiskit.visualization import plot_histogram
+from collections import Counter
--- readme.txt
+++ readme.txt
@@ -1,1 +1,6 @@
-asdas
+Variational Quantum Classifier, identifying _______.
+
+Lines of Code:
+Hours Spent:
+
+Read this file because etc. etc. yap yap yap

1fe475ccf20af6d487bbda40f4abf420531a4e41
aruld <aruldmoorthyrams@gmail.com>
2024-07-23 16:49:55 -0400

    adas

---
+++ main.py
@@ -0,0 +1,5 @@
+from qiskit import QuantumCircuit, QuantumRegister
+from qiskit.circuit.library import QFT
+from qiskit.circuit import ControlledGate
+from qiskit.quantum_info import Statevector
+from qiskit_aer import AerSimulator

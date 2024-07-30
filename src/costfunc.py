62c24ffa8e1806ec2c45fc4fb3046abbd78ed83c
Ananya Tadepalli <ananyatadepalli@Ananyas-MBP-3.hsd1.ga.comcast.net>
2024-07-30 12:02:53 -0400

    added optimization

--- src/costfunc.py
+++ src/costfunc.py
@@ -34,3 +34,39 @@ def customCost(predictions, labels, data):
                   linear_online_order)
     
     return cost
+
+def compute_gradient(prediction, label, b, is_log):
+    if is_log:
+        if label == 0:
+            return 1 / np.log(b)
+        elif label == 1:
+            return -1 / np.log(b)
+    else:
+        if label == 0:
+            return 1 / 2
+        elif label == 1:
+            return -1 / 2
+        
+def update_parameters(params, gradients, learning_rate):
+    return params - learning_rate * gradients
+
+
+
+def optimize_vqc(params, data, labels, learning_rate, b, num_iterations):
+    
+    for iteration in range(num_iterations):
+        #this is where we call the function to apply the gates and get back the measurement?
+        #predictions = quantum_circuit(params, data)
+        gradients = np.zeros_like(params)
+
+        # Compute gradients
+        for i in range(len(data)):
+            for j in range(predictions.shape[1]):
+                is_log = j < 2  
+                gradient = compute_gradient(predictions[i, j], labels[i], b, is_log)
+                gradients[j] += gradient
+
+        cost = customCost(predictions, labels, data)
+        params = update_parameters(params, gradients, learning_rate)
+    
+    return params

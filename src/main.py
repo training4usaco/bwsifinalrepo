4d2b61cb7bd18227fa5c2b5e38a817f8f72a0f84
training4usaco <103396810+training4usaco@users.noreply.github.com>
2024-07-31 12:42:27 -0700

    added initial_guess to main

--- src/main.py
+++ src/main.py
@@ -3,11 +3,11 @@ from autoEncoder import AutoEncoder
 
 CATEGORIES = 7
 
-def main():
+def VQC():
     (normal_data, fraud_data)  = encodeData.EncodeData(CATEGORIES, "../data/test_data.csv")
     theta_list = [0] * normal_data.num_qubits    # NOTE: temporary
-    # AutoEncoder(normal_data, CATEGORIES, theta_list)
+    initial_guess = AutoEncoder(normal_data, CATEGORIES, theta_list)
 
-if __name__ == "__main__":
-    main()
+
+
 

89977f1f971ba3326d93462f78d3a157460f8687
aruld <aruldmoorthyrams@gmail.com>
2024-07-31 15:18:53 -0400

    main program starting

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
-import encodeData
+import encodeData as ED
+from ansatz import Ansatz
+import autoEncoder as AE
+import costfunc as CF
+
 from autoEncoder import AutoEncoder
+
+
+import numpy as np
 
 CATEGORIES = 7
 
 def main():
-    (normal_data, fraud_data)  = encodeData.EncodeData(CATEGORIES, "../data/test_data.csv")
-    theta_list = [0] * normal_data.num_qubits    # NOTE: temporary
+    (normal_data, fraud_data)  = ED.EncodeData(CATEGORIES, "../data/test_data.csv")
+    theta_list = [np.random(0, np.pi)] * CATEGORIES
+    Ansatz()
+       # NOTE: temporary
     # AutoEncoder(normal_data, CATEGORIES, theta_list)
 
 if __name__ == "__main__":

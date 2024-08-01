27570e4ce33ada18619a6d9e8d69c0779c78ab10
aruld <aruldmoorthyrams@gmail.com>
2024-08-01 18:48:36 -0400

    d

--- src/autoEncode.py
+++ src/autoEncode.py
@@ -1,45 +1,45 @@
-from qiskit import QuantumCircuit
-from qiskit import ClassicalRegister, QuantumRegister, Parameter
-from qiskit_aer import Aer
-import math
-import numpy as np
+# from qiskit import QuantumCircuit
+# from qiskit import ClassicalRegister, QuantumRegister, Parameter
+# from qiskit_aer import Aer
+# import math
+# import numpy as np
 
-'''
-Functionality of autoencoder from paper:
-- data compression: reduces input to less qubits (only latent qubits left after)
-- unitary, but no decoder needed?
-- "parameterized quantum circuit" 
-- minimize reconstruction error
-- need classical register + auxiliary qubit + reference space
-'''
+# '''
+# Functionality of autoencoder from paper:
+# - data compression: reduces input to less qubits (only latent qubits left after)
+# - unitary, but no decoder needed?
+# - "parameterized quantum circuit" 
+# - minimize reconstruction error
+# - need classical register + auxiliary qubit + reference space
+# '''
 
-# n = total number of qubits
-# nt = number of trash qubits
-# accordign to diagram in paper:
-# i think it goes like (ry gates on all qubits -> cz gates on some pairs) x nt 
-# -> ry gates on trash qubits at the end 
+# # n = total number of qubits
+# # nt = number of trash qubits
+# # accordign to diagram in paper:
+# # i think it goes like (ry gates on all qubits -> cz gates on some pairs) x nt 
+# # -> ry gates on trash qubits at the end 
 
-def ansatz(n, nt): 
-    qc = QuantumCircuit(n)
-    theta = [Parameter(f'θ{i}') for i in range(n)]
+# def ansatz(n, nt): 
+#     qc = QuantumCircuit(n)
+#     theta = [Parameter(f'θ{i}') for i in range(n)]
 
-    for i in range(nt):          
-        # ry gates on all qubits
-        for i in range(n):
-            qc.ry(theta[i], i)
+#     for i in range(nt):          
+#         # ry gates on all qubits
+#         for i in range(n):
+#             qc.ry(theta[i], i)
     
 
-    # cz gates on trash and latent qubits
+#     # cz gates on trash and latent qubits
     
 
 
-    # ry gates on trash qubits
-    for i in range(nt):
-        qc.ry(theta[i], i)
+#     # ry gates on trash qubits
+#     for i in range(nt):
+#         qc.ry(theta[i], i)
     
     
 
-# function should be applied after classical data is converted to quantum qubits 
-def autoEncode(trash, latent):
-    reg = QuantumRegister()
+# # function should be applied after classical data is converted to quantum qubits 
+# def autoEncode(trash, latent):
+#     reg = QuantumRegister()
 
